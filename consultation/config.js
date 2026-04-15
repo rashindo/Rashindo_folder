35af27e2dfa4dad3c2ee1c117bd12db017b273d8
rashindo <bcyp1985@yahoo.co.jp>
2026-04-15 20:03:33 +0900

    consultation本番用config.js追加と読み込み順を確定

--- consultation/config.js
+++ consultation/config.js
@@ -1,6 +1,6 @@
 window.CONSULTATION_CONFIG = {
   ENDPOINTS: {
-    GAS_WEBAPP_URL: "https://script.google.com/macros/s/REPLACE_WITH_DEPLOY_ID/exec"
+    GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbz1UaccBYrzasI5R3URixNwjlzwsE2paST_fVlAi3HGDgZu1xyw5sw9z9scy3jTNzI-/exec"
   },
   // 既存形式との互換用（ENDPOINTS.GAS_WEBAPP_URL が優先）
   FORM_ENDPOINT: "https://script.google.com/macros/s/AKfycbz1UaccBYrzasI5R3URixNwjlzwsE2paST_fVlAi3HGDgZu1xyw5sw9z9scy3jTNzI-/exec",
